bf88b10baca22d37e581e8b97e21b45902d116c4
oliverfeher <oliver92.feher@gmail.com>
2020-04-21 02:27:08 -0400

    Done.

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ BoardMember.prototype.veto = function()
 
 BoardMember.prototype.approve = function()
 {
-    return "You can do that";
+    return "You can do that!";
 }
 
 BoardMember.prototype.doCharity = function()
 {
-    return "I like to help people";
+    return "I like to help people.";
 }
 
 BoardMember.prototype.releasePressStatement = function()
 {
-    return "You will see great hings from Scuber";
+    return "You will see great things from Scuber.";
 }
 
 BoardMember.prototype.sayHi = function()
 {
-    `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`;
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 }
 
